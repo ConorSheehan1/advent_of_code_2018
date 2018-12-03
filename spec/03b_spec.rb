e0318f3e6c34350c6d3ae0debab6c67a82adb581
conors_nli <conor.sheehan.2@ucdconnect.ie>
2018-12-03 23:14:04 +0000

    03b done

--- spec/03b_spec.rb
+++ spec/03b_spec.rb
@@ -3,7 +3,8 @@
 require_relative "../src/03b.rb"
 
 describe "day_03b" do
-  it "should do something" do
-
+  it "should find the id with no overlap" do
+    arr = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]
+    expect(day_03b(arr)).to eq("#3")
   end
 end
--- src/03b.rb
+++ src/03b.rb
@@ -3,10 +3,33 @@
 require_relative "./03a"
 
 def day_03b(arr)
-  coords = arr.map do |v| 
+  coords = []
+  arr.each do |coord_string| 
     # convert string to hash
-    coords = str_coords_to_hash(v)
+    id_hash = str_coords_to_hash(coord_string)
     # add full coordinates to hash
-    coords[:coords] = xywh_to_arr(coords)
+    id_hash[:coords] = xywh_to_arr(id_hash)
+    coords << id_hash
+  end
+  coord_hash = {}
+  coords.each do |coord_with_id|
+    coord_with_id[:coords].each do |coord|
+      if coord_hash[coord]
+        coord_hash[coord] += 1
+      else
+        coord_hash[coord] = 1
+      end
+    end
+  end
+  # coord_hash.select {|k,v| k if v > 1}.uniq.count
+  overlap = Set.new
+  coord_hash.map do |k,v|
+    overlap << k if v > 1
+  end
+  overlap = overlap.to_a
+  coords.each do |coord_with_id|
+    # [] & [] get intersection of arrays
+    # return id where there is no overlap
+    return coord_with_id[:id] if (coord_with_id[:coords] & overlap).empty?
   end
 end
